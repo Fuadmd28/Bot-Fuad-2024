12a823a8da2819e2477c2342fabbb66fdb1b6f52
Fuadmd28 <110433657+Fuadmd28@users.noreply.github.com>
2024-02-29 11:56:30 +0700

    Update _info-sewa.js

--- plugins/_info-sewa.js
+++ plugins/_info-sewa.js
@@ -1,3 +1,9 @@
+import fs from 'fs'
+
+let handler = async (m, { conn }) => {
+	let rules = `
+_Untuk Mau Memasukan Bot  Kedalam Group, Kamu Bisa Mensewa Nya Harga Mumer_
+
 ╭––『 *SEWA BOT*  』
 ┆➭ 7 Hari *Rp 3,000*
 ┆➭ 1 Bulan *Rp 10,000*
@@ -16,3 +22,11 @@ https://wa.me/p/6331501620289183/6282147781510
 
 *_Minat Hub_*
 wa.me/6282147781510
+`;
+	await conn.sendMessage(m.chat, { image: { url: 'https://telegra.ph/file/f9da49558d8aa6ea5031c.jpg' }, caption: rules }, m)
+}
+handler.help = ['sewa']
+handler.tags = ['vynaamenu']
+handler.command = /^(sewa|belibot)$/i;
+
+export default handler;
